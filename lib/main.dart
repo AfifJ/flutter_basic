5a907005c5692263bb9ea536e19ce9b6dd245809
AfifJ <afifjamhari.id@gmail.com>
2024-04-25 20:35:04 +0700

    button and icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-// import 'dart:ui';
+import 'dart:ui';
 
-// import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const MaterialApp(home: Home()));
 
@@ -13,29 +15,44 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: const Text("First Flutter App",
-        style: TextStyle(
-          fontFamily: "IndieFlower"
-        ),),
+        title: const Text(
+          "First Flutter App",
+          style: TextStyle(
+            fontSize: 24.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
       ),
       body: Center(
-          child: Image.asset("assets/space-2.jpg")
-          // Text(
-          //   "Hallo semua",
-          //   style: TextStyle(
-          //       fontSize: 20,
-          //       fontWeight: FontWeight.bold,
-          //       color: Colors.red[800]),
+          // lesson 9 - button and icons
+          child: IconButton(onPressed: () {}, icon: const Icon(Icons.mail))
+
+          // ElevatedButton.icon(
+          //     onPressed: () {},
+          //     icon: const Icon(Icons.mail),
+          //     label: const Text("test"))
+
+          // TextButton(
+          //   onPressed: () {
+          //     print("test");
+          //   },
+          //   style: TextButton.styleFrom(
+          //     backgroundColor: Colors.red[100],
+          //   ),
+          //   child: const Text("test"),
+          // )
+
+          // Icon(
+          //   Icons.add_circle,
+          //   color: Colors.lightBlue,
+          //   size: 100.0,
           // ),
           ),
       floatingActionButton: FloatingActionButton(
         child: const Text(
           "Click",
-          style: TextStyle(
-            fontFamily: "IndieFlower",
-            fontSize: 20,
-          ),
         ),
         onPressed: () {},
       ),
